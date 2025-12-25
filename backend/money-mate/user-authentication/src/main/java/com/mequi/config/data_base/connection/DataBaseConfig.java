92ea4be9098f5e1273e02ae5d1908ceb9c12d830
renatograca <renatomacedo1995@gmail.com>
2025-12-25 19:36:49 -0300

    Make database configuration values configurable via environment variables

--- backend/money-mate/user-authentication/src/main/java/com/mequi/config/data_base/connection/DataBaseConfig.java
+++ backend/money-mate/user-authentication/src/main/java/com/mequi/config/data_base/connection/DataBaseConfig.java
@@ -2,18 +2,16 @@ package com.mequi.config.data_base.connection;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
-import java.io.InputStream;
-import java.util.Properties;
 import javax.sql.DataSource;
 
 public class DataBaseConfig {
   public static DataSource getDataSource() {
     final var config = new HikariConfig();
-    config.setJdbcUrl("jdbc:postgresql://localhost:5432/user_db");
-    config.setUsername("postgres");
-    config.setPassword("postgres");
+    config.setJdbcUrl(System.getenv().getOrDefault("DB_URL", "jdbc:postgresql://localhost:5432/user_db"));
+    config.setUsername(System.getenv().getOrDefault("DB_USER", "postgres"));
+    config.setPassword(System.getenv().getOrDefault("DB_PASSWORD", "postgres"));
     config.setDriverClassName("org.postgresql.Driver");
-    config.setMaximumPoolSize(10);
+    config.setMaximumPoolSize(Integer.parseInt(System.getenv().getOrDefault("DB_MAX_POOL_SIZE", "10")));
     return new HikariDataSource(config);
   }
 }
